1373df86fafeac994b030f16d8846cff92b7999b
Alina Dryzhynskaya <adrizhinskaya@bk.ru>
2023-10-07 17:06:20 +0300

    Исправлен bestSeries

--- src/MonthData.java
+++ src/MonthData.java
@@ -3,7 +3,7 @@ public class MonthData {
 
     void printDaysAndStepsFromMonth() {
         for (int i = 0; i < days.length; i++) {
-            System.out.println((i+1) + " день: " + days[i]);
+            System.out.println((i + 1) + " день: " + days[i]);
         }
     }
 
@@ -25,15 +25,16 @@ public class MonthData {
 
     int bestSeries(int goalByStepsPerDay) {
         int currentSeries = 0, finalSeries = 0;
-        for (int i = 0; i < days.length; i++) {
-            if (days[i] >= goalByStepsPerDay) {
+        for (int day : days) {
+            if (day >= goalByStepsPerDay) {
                 currentSeries++;
-            }
-            if (days[i] < goalByStepsPerDay || (days[i] >= goalByStepsPerDay && i == days.length - 1)) {
                 if (currentSeries > finalSeries) {
                     finalSeries = currentSeries;
-                    currentSeries = 0;
                 }
+            }
+            if (day < goalByStepsPerDay) {
+                finalSeries = currentSeries;
+                currentSeries = 0;
             }
         }
         return finalSeries;
